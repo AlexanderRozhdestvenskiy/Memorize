edadae0f52ac28fe3a2a92dd324b5648bed4edc3
AlexanderRozhdestvenskiy <alexrozh1988@gmail.com>
2020-09-05 14:43:29 +0300

    Full 6

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -12,14 +12,23 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        Grid(viewModel.cards) { card in
-            CardView(card: card).onTapGesture {
-                self.viewModel.choose(card: card)
+        VStack {
+            Grid(viewModel.cards) { card in
+                CardView(card: card).onTapGesture {
+                    withAnimation(.linear) {
+                        self.viewModel.choose(card: card)
+                    }
+                }
+                    .padding(5)
             }
-        .padding(5)
+                .padding()
+                .foregroundColor(Color.orange)
+            Button(action: {
+                withAnimation(.easeInOut(duration: 1)) {
+                    self.viewModel.resetGame()
+                }
+            }, label: { Text("New Game") })
         }
-            .padding()
-            .foregroundColor(Color.orange)
     }
 }
 
@@ -40,8 +49,11 @@ struct CardView: View {
                     .padding(5).opacity(0.4)
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
             }
             .cardify(isFaceUp: card.isFaceUp)
+            .transition(AnyTransition.scale)
         }
     }
     
@@ -52,32 +64,7 @@ struct CardView: View {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // MARK: - Prewiew
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
